f16252531817478d1dde2a1da7b2fb62a94121f4
Jasurbek <jasurdev1604@gmail.com>
2026-09-14 14:28:01 +0500

    Use updated Sheets status web app endpoint

--- dist/config.js
+++ dist/config.js
@@ -1,2 +1,2 @@
 // Google Apps Script Web App URL for the Ustoz Hasan Leadlar sheet.
-window.LEAD_CONFIG = Object.freeze({ endpoint: 'https://script.google.com/macros/s/AKfycbzqONbw1sx5zrt2cRGEViu_yonD84kQWb-BJ1ENIKfrKTJlnOF9pSyQbSSFRxEDPU22-A/exec' });
+window.LEAD_CONFIG = Object.freeze({ endpoint: 'https://script.google.com/macros/s/AKfycbzh09YvDj9ZGTab6NKU-Xy0FrqnLewMnuwlvQwUQvPbt0TY_FMuuMO8d-cNFgKSCvDC/exec' });
