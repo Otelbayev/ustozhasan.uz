055d4c7095d93c9a60c6d10908a3152252e826c1
Jasurbek <jasurdev1604@gmail.com>
2026-09-14 14:44:03 +0500

    Use verified Sheets lead endpoint

--- dist/config.js
+++ dist/config.js
@@ -1,2 +1,2 @@
 // Google Apps Script Web App URL for the Ustoz Hasan Leadlar sheet.
-window.LEAD_CONFIG = Object.freeze({ endpoint: 'https://script.google.com/macros/s/AKfycbwfI2qa_k1043lfXBV1fuoK9EE17UY1x8kUZWa1cYJBsIErkcBiQztIrLPLBPDocTBrYQ/exec' });
+window.LEAD_CONFIG = Object.freeze({ endpoint: 'https://script.google.com/macros/s/AKfycbwQeUZgQf7Xc7UaGAn4qaCUUkCZB9caqMbpqTFq9Ms-pjNd4VUSyzdsvzBL3Qe8-blIKQ/exec' });
