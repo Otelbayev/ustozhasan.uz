021a871d851105f16d73585940a0428490e35aba
Jasurbek <jasurdev1604@gmail.com>
2026-09-14 14:39:21 +0500

    Point leads form to version 3 Sheets deployment

--- dist/config.js
+++ dist/config.js
@@ -1,2 +1,2 @@
 // Google Apps Script Web App URL for the Ustoz Hasan Leadlar sheet.
-window.LEAD_CONFIG = Object.freeze({ endpoint: 'https://script.google.com/macros/s/AKfycbzh09YvDj9ZGTab6NKU-Xy0FrqnLewMnuwlvQwUQvPbt0TY_FMuuMO8d-cNFgKSCvDC/exec' });
+window.LEAD_CONFIG = Object.freeze({ endpoint: 'https://script.google.com/macros/s/AKfycbwfI2qa_k1043lfXBV1fuoK9EE17UY1x8kUZWa1cYJBsIErkcBiQztIrLPLBPDocTBrYQ/exec' });
